f41fb2d276390a1ca67c53d40aa9b586f368283e
Larry's Mac <larry.c.chien@gmail.com>
2018-09-24 00:58:19 +0800

    Build presentational components

--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-// import todoApp from './reducers'
-// import App from './components/App'
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers/reducer';
+import App from './components/app.jsx';
 
-// let store = createStore(todoApp);
+let store = createStore(todoApp);
 
 render(
-    <Provider>
+    <Provider store={store}>
         Hello
     </Provider>,
     document.getElementById('content')
