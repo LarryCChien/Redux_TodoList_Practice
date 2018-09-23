bb0a251f5bc1e9d98b7ba3606cbf63be473cdcf6
Larry's Mac <larry.c.chien@gmail.com>
2018-09-24 00:57:00 +0800

    Build store

---
+++ app/reducers/state.js
@@ -0,0 +1,10 @@
+export default defaultState = {
+    visibilityFilter: 'SHOW_ALL',
+    todos: [{
+        text: '這個預設有打勾',
+        checked: true,
+    }, {
+        text: '這個預設沒有打勾',
+        checked: false,
+    }]
+};
---
+++ app/store/store.js
@@ -0,0 +1,6 @@
+import { createStore } from '../../node_modules/redux';
+import { todoApp } from '../reducers/reducer';
+import { defaultState } from '../reducers/state';
+
+// 預設值是 從 state.js 傳來的 defaultState 。
+let store = createStore(todoApp, defaultState);
