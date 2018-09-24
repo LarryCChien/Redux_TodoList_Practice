bd253b45e8d62794f625f66b716bc70e290c6174
Larry's Mac <larry.c.chien@gmail.com>
2018-09-24 17:03:27 +0800

    Remove files no one use

--- app/store/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import { todoApp } from '../reducers/reducer';
-import { defaultState } from '../reducers/state';
-
-// 預設值是 從 state.js 傳來的 defaultState 。
-let store = createStore(todoApp, defaultState);
